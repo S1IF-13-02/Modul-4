edf2ec0f503ef0611cb884df334ea66df34de8bd
rafiramadhan <rafirama1020@gmail.com>
2025-10-08 14:05:52 +0700

    Update guide1.go

--- Rafi Ramadhan/guide1.go
+++ Rafi Ramadhan/guide1.go
@@ -1,1 +1,15 @@
+package main
+ 
+import "fmt"
 
+func main() {
+	var d int
+
+	fmt.Print("Detik: ")
+	fmt.Scanln(&d)
+	 j := d / 3600
+	 sisa := d % 3600
+	 m := sisa / 60
+	 s := sisa % 60
+	 fmt.Printf("%d jam %d menit dan %d detik\n", j, m, s)
+}
