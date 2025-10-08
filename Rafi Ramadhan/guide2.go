9a3df933e1522d56cb34aa4803cdbfb77020a324
rafiramadhan <rafirama1020@gmail.com>
2025-10-08 14:06:33 +0700

    Create guide2.go

---
+++ Rafi Ramadhan/guide2.go
@@ -0,0 +1,13 @@
+package main
+import "fmt"
+func main () {
+	var bilangan int
+	var result bool
+	fmt.Print("Masukan bilangan bulat: ")
+	fmt.Scanln(&bilangan)
+	d1 := bilangan / 1000
+	d2 := (bilangan / 100) % 10
+	d3 := (bilangan / 10) % 10
+	result = d1 <= d2 && d2 <= d3
+	fmt.Println(result)
+}
